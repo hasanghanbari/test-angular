2b0bdf8c78794997beda7cc4dedac4e60f033b18
hasan ghanbari <hasanghanbari20@gmail.com>
2022-04-30 21:51:38 +0430

    Add PDF Downloader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'maher';
+
+  public exportHtmlToPDF(){
+    let data = document.getElementById('page');
+
+    if (data) {
+      html2canvas(data).then(canvas => {
+
+        let docWidth = 208;
+        let docHeight = canvas.height * docWidth / canvas.width;
+
+        const contentDataURL = canvas.toDataURL('image/png')
+        let doc = new jsPDF('p', 'mm', 'a4');
+        let position = 0;
+        doc.addImage(contentDataURL, 'PNG', 0, position, docWidth, docHeight)
+
+        doc.save('exportedPdf.pdf');
+      });
+    }
+  }
 }
